b836990f778c940be636be9bc0fc3c6f0a53b821
forever sumit <forever.sumitsharma@gmail.com>
2013-09-06 02:25:52 +0530

    configure rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gurjar::Application.config.secret_key_base = 'a3cb84bc69b3c6b13d1ffd19d6ebe81046439f750b012e6b9514829f57653ac2c686b16e5b497d4c279dbad09a895dafe89c3d026ab0a1d8edf8127c3724b482'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gurjar::Application.config.session_store :cookie_store, key: '_gurjar_session'
